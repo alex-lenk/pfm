6423f7fb2b18f0c60c4ae7ba1e7cc91499733296
artlenk.ru@gmail.com <df5ukoi7ht>
2018-04-20 23:18:39 +0300

    Initial commit

---
+++ src/js/lib.js
@@ -0,0 +1,45 @@
+/*
+ *
+ * Content of libraries
+ * 1.   smoothscroll.js
+ * 2.   svgxuse.min.js
+ * 3.   jquery.everslider.min.js
+ * 4.   swiper.min.js
+ * 5.   jquery.formstyler.min.js
+ * 6.   isotope.pkgd.min.js
+ * 7.   jquery.fancybox.min.js
+ * 8.   bootstrap.min.js
+ * 9.   current-device.min.js
+ *
+ */
+
+/* 1. smoothscroll.js */
+//= lib/smoothscroll.js
+
+
+/* 2. svgxuse.min.js */
+//= lib/svgxuse.min.js
+
+
+/* 3. owl.carousel.min.js */
+//= ../../bower_components/owl.carousel/dist/owl.carousel.min.js
+
+
+/* 4. swiper.min.js */
+// = ../../bower_components/swiper/dist/js/swiper.min.js
+
+
+/* 5. jquery.formstyler.min.js */
+// = lib/jquery.formstyler.min.js
+
+/* 6. slick.min.js */
+//= ../../bower_components/slick-carousel/slick/slick.min.js
+
+/* 7. jquery.fancybox.min.js */
+//= ../../bower_components/fancybox/dist/jquery.fancybox.min.js
+
+/* 8. bootstrap.min.js */
+//= lib/bootstrap.min.js
+
+/* 9. bootstrap.min.js */
+//= lib/jquery.mb.browser.min.js
