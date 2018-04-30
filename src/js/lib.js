618f7bef9be3ff97a2e206b69f2bd7ae06423b72
artlenk.ru@gmail.com <df5ukoi7ht>
2018-04-30 08:03:19 +0300

    Fixed minor bugs and created mobile menu

--- src/js/lib.js
+++ src/js/lib.js
@@ -3,13 +3,9 @@
  * Content of libraries
  * 1.   smoothscroll.js
  * 2.   svgxuse.min.js
- * 3.   jquery.everslider.min.js
- * 4.   swiper.min.js
- * 5.   jquery.formstyler.min.js
- * 6.   isotope.pkgd.min.js
- * 7.   jquery.fancybox.min.js
- * 8.   bootstrap.min.js
- * 9.   current-device.min.js
+ * 3.   slick.min.js
+ * 4.   stickUp.min.js
+ * 5.   bootstrap.min.js
  *
  */
 
@@ -22,10 +18,8 @@
 /* 3. slick.min.js */
 //= ../../bower_components/slick-carousel/slick/slick.min.js
 
-/* 4. jquery.fancybox.min.js */
-// = ../../bower_components/fancybox/dist/jquery.fancybox.min.js
+/* 4. stickUp.min.js */
+//= lib/stickUp.min.js
 
 /* 5. bootstrap.min.js */
 //= lib/bootstrap.min.js
-
-//= lib/stickUp.min.js
