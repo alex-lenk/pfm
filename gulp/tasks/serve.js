8803b261013ca8843c5203fb6d4b70d30a49b19a
alex-lenk <alexlenk.ru@gmail.com>
2020-12-15 17:18:28 +0300

    Created section blocks

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -10,8 +10,8 @@ const copyDependencies = require('./copyDependencies')
 const server = require('browser-sync').create()
 
 function readyReload(cb) {
-  server.reload()
-  cb()
+    server.reload()
+    cb()
 }
 
 module.exports = function serve(cb) {
@@ -26,7 +26,7 @@ module.exports = function serve(cb) {
     gulp.watch('src/img/sprite/*.svg', gulp.series(svgSprite, readyReload))
     gulp.watch('src/styles/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)))
     gulp.watch('src/js/**/*.js', gulp.series(script, readyReload))
-    gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload))
+    gulp.watch('src/pages/**/*.twig', gulp.series(pug2html, readyReload))
 
     gulp.watch('package.json', gulp.series(copyDependencies, readyReload))
 
--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,13 +1,23 @@
 const gulp = require('gulp')
 const svgstore = require('gulp-svgstore')
+const svgmin = require('gulp-svgmin');
+const basePath = require('path');
 const rename = require('gulp-rename')
 
 module.exports = function svgSprite() {
-  return gulp.src('src/img/icons/*.svg')
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename('sprite.svg'))
-    .pipe(gulp.dest('build/img'))
+    return gulp.src('src/img/icons/*.svg')
+        .pipe(svgmin(function (file) {
+            let prefix = basePath.basename(file.relative, basePath.extname(file.relative));
+            return {
+                plugins: [{
+                    cleanupIDs: {
+                        prefix: prefix + '-',
+                        minify: true
+                    }
+                }]
+            }
+        }))
+        .pipe(svgstore())
+        .pipe(rename('sprite.svg'))
+        .pipe(gulp.dest('build/img'))
 }
-
