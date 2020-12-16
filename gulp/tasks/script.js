65265b653ed025008a1a88b7204e7b42def2ae29
alex-lenk <alexlenk.ru@gmail.com>
2020-12-17 00:11:32 +0300

    Created partners-carousel case teaching and footer blocks

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -1,37 +1,18 @@
-const gulp = require('gulp')
-const plumber = require('gulp-plumber')
-const webpack = require('webpack-stream')
-const CircularDependencyPlugin = require('circular-dependency-plugin')
-const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin")
-const eslint = require('gulp-eslint')
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const webpack = require('webpack-stream');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const DuplicatePackageCheckerPlugin = require("duplicate-package-checker-webpack-plugin");
+const eslint = require('gulp-eslint');
+const rigger = require('gulp-rigger');
+const uglify = require('gulp-uglify-es').default;
+const rename = require('gulp-rename');
 
 module.exports = function script() {
-  return gulp.src('src/js/main.js')
-    .pipe(plumber())
-    .pipe(webpack({
-      mode: process.env.NODE_ENV,
-      output: {
-        filename: '[name].min.js',
-      },
-      module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
-          }
-        ]
-      },
-      plugins: [
-        new CircularDependencyPlugin(),
-        new DuplicatePackageCheckerPlugin()
-      ]
-    }))
-    .pipe(gulp.dest('build/js'));
+    return gulp.src('src/js/main.js')
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(gulp.dest('build/js'));
 }
-
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,21 @@
-import $ from '../local_modules/jquery/dist/jquery.min'
+'use strict';
+//= vendor/jquery.min.js
+//= vendor/slick.min.js
 
-$(document).ready(() => {
-    // eslint-disable-next-line no-console
-    console.log(`document ready`)
+$(document).ready(function () {
+    console.log(`document ready`);
+
+    $('.partners-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="partners-carousel__arrow partners-carousel__prev slick-prev"></button>',
+        nextArrow: '<button type="button" class="partners-carousel__arrow partners-carousel__next slick-next"></button>',
+        responsive: [{
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
 })
