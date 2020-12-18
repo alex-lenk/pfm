1f7969556943ffe2f63049f913d04cabcb6a34a4
alex-lenk <alexlenk.ru@gmail.com>
2020-12-19 02:44:15 +0300

    Created fixed header block

--- src/js/main.js
+++ src/js/main.js
@@ -26,7 +26,39 @@ $(document).ready(function () {
         $('body').removeClass('menu-open');
     });
 
+    /* BEGIN: Плавная прокрутка к ID */
+    let js__goAnchor = '.js__go-anchor';
+    $(js__goAnchor).on('click', function (e) {
+        e.preventDefault();
+        let fixed_offset = 10;
+        $('html, body').stop().animate({scrollTop: $(this.hash).offset().top - fixed_offset}, 900);
+    });
+    /* END */
+
+
     if ($(window).width() < 575) {
         $('.header-phone').appendTo('.menu-wrap');
+
+        $(js__goAnchor).click(function () {
+            $('body').removeClass('menu-open');
+        });
     }
+
+
+    /* BEGIN: Инициализация плавающего блока на десктопной версия странице */
+    if ($(window).width() > 576) {
+        var elementFixed = '.menu-wrap';
+    } else {
+        var elementFixed = '.header-top';
+    }
+    var elementPosition = $(elementFixed).offset();
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > elementPosition.top) {
+            $(elementFixed).addClass("menu-fixed");
+        } else {
+            $(elementFixed).removeClass("menu-fixed");
+        }
+    });
+    /* END */
 });
