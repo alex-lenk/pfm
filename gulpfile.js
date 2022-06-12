d982e5c760503da4b332f046cd8ce8f859577f86
AlexLenk <alexlenk.ru@gmail.com>
2022-06-12 16:45:05 +0300

    Refactoring gulp task runner

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,221 @@
-const gulp = require('gulp')
+'use strict'
+import gulp from 'gulp'
 
-const serve = require('./gulp/tasks/serve')
-const pug2html = require('./gulp/tasks/pug2html')
-const styles = require('./gulp/tasks/styles')
-const script = require('./gulp/tasks/script')
-const fonts = require('./gulp/tasks/fonts')
-const imageMinify = require('./gulp/tasks/imageMinify')
-const clean = require('./gulp/tasks/clean')
-const copyDependencies = require('./gulp/tasks/copyDependencies')
-const lighthouse = require('./gulp/tasks/lighthouse')
-const svgSprite = require('./gulp/tasks/svgSprite')
+const {series, watch, src, dest, lastRun} = gulp
 
-function setMode(isProduction = false) {
-  return cb => {
-    process.env.NODE_ENV = isProduction ? 'production' : 'development'
-    cb()
+import twig from 'gulp-twig'
+import autoprefixer from 'gulp-autoprefixer'
+import cleanCSS from 'gulp-clean-css'
+import webpack from 'webpack'
+import webpackStream from 'webpack-stream'
+import browserSync from 'browser-sync'
+import svgMin from 'gulp-svgmin'
+import svgStore from 'gulp-svgstore'
+import basePath from 'path'
+import newer from 'gulp-newer'
+import rename from 'gulp-rename'
+import debug from 'gulp-debug'
+import imagemin from 'gulp-imagemin'
+import imageminOptiPng from 'imagemin-optipng'
+import imageminMozJpeg from 'imagemin-mozjpeg'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass(dartSass)
+
+const pathFiles = {
+  html: {
+    src: ['./src/pages/**/*.twig', '!./src/pages/components/*.twig'],
+    build: './build',
+    watch: './src/pages/**/*.twig'
+  },
+  css: {
+    src: ['./src/styles/**/**/*.scss', '!./src/styles/vendors/**'],
+    build: './build/assets/css/',
+    watch: './src/styles/**/**/*.scss'
+  },
+  js: {
+    src: ['./src/js/**/!(_)*.js', '!./src/js/vendor/'],
+    srcVendor: './src/js/vendor.js',
+    build: './build/assets/js/',
+    watch: './src/js/**/*.js'
+  },
+  img: {
+    src: './src/img/**/*.+(png|jpg|jpeg|svg)',
+    build: './build/assets/img/',
+    watch: './src/img/**/*.+(png|jpg|jpeg|svg)'
+  },
+  fonts: {
+    src: './src/fonts/*.*',
+    build: './build/assets/fonts/',
+    watch: './src/fonts/*.**'
+  },
+  favicon: {
+    src: './src/favicon/*.*',
+    build: './build/assets/favicon/',
+    watch: './src/favicon/*.**'
   }
 }
 
-const dev = gulp.parallel(pug2html, styles, script, fonts, imageMinify, svgSprite)
+const HTML = () => {
+  return src(pathFiles.html.src)
+    .pipe(twig())
+    .pipe(dest(pathFiles.html.build))
+    .pipe(browserSync.stream())
+}
 
-const build = gulp.series(copyDependencies, dev)
+const SCSS = () => {
+  return src(pathFiles.css.src)
+    //.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(rename({
+      suffix: ".min"
+    }))
+    .pipe(cleanCSS({
+      level: {
+        1: {
+          specialComments: 0
+        }
+      }
+    }))
+    //.pipe(sourcemaps.write('maps'))
+    .pipe(dest(pathFiles.css.build))
+    .pipe(browserSync.stream())
+}
 
-module.exports.start = gulp.series(setMode(), build, serve)
-module.exports.build = gulp.series(setMode(true), build)
+const js = () => {
+  return src(pathFiles.js.src, {since: lastRun(HTML)})
+    .pipe(webpackStream({
+      mode: "production",
+      entry: {
+        "scripts": "./src/js/scripts.js",
+        "scripts.min": "./src/js/scripts.js"
+      },
+      performance: {
+        hints: false,
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+      },
+      output: {
+        filename: "[name].js"
+      },
+      module: {
+        rules: [{
+          test: /\.js$/,
+          exclude: /^_(\w+)(\.js)$|node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }]
+      },
+      optimization: {
+        minimizer: [],
+      },
+      plugins: []
+    }), webpack)
+    .pipe(dest(pathFiles.js.build))
+    .pipe(browserSync.stream())
+}
 
-module.exports.lighthouse = gulp.series(lighthouse)
+/*
+const jsVendor = () => {
+  return src(pathFiles.js.srcVendor)
+    .pipe(rigger())
+    //.pipe(uglify())
+    .pipe(dest(pathFiles.js.build))
+    .pipe(browserSync.stream())
+}
+*/
+
+const images = () => {
+  return src(pathFiles.img.src)
+    .pipe(newer(pathFiles.img.build))
+    .pipe(imagemin([
+      imageminOptiPng({
+        optimizationLevel: 5
+      }),
+      imageminMozJpeg({
+        progressive: true,
+        quality: 85
+      })
+    ]))
+    .pipe(dest(pathFiles.img.build))
+    .pipe(debug({
+      "title": "Images:"
+    }))
+    .on("end", browserSync.reload);
+}
+
+const fonts = () => {
+  return src(pathFiles.fonts.src)
+    .pipe(dest(pathFiles.fonts.build))
+    .pipe(debug({
+      "title": "Fonts:"
+    }))
+    .pipe(browserSync.stream())
+}
+
+const favicon = () => {
+  return src(pathFiles.favicon.src)
+    .pipe(dest(pathFiles.favicon.build))
+    .pipe(debug({
+      "title": "Favicon:"
+    }))
+    .pipe(browserSync.stream())
+}
+
+const myServer = () => {
+  browserSync.init({
+    server: {
+      baseDir: pathFiles.html.build
+    },
+    port: 4004,
+    notify: false,
+    tunnel: true
+  })
+  watch(pathFiles.html.watch, {usePolling: true}, HTML);
+  watch(pathFiles.css.watch, SCSS);
+  watch(pathFiles.js.watch, js);
+  //watch(pathFiles.js.watch, jsVendor);
+}
+
+const svgSprite = () => {
+  return src('./src/img/sprite/*.svg')
+    .pipe(svgMin(function getOptions(file) {
+      let prefix = basePath.basename(
+        file.relative,
+        basePath.extname(file.relative)
+      );
+      return {
+        plugins: [
+          'removeDoctype',
+          'removeComments',
+          'sortAttrs',
+          {
+            name: 'cleanupIDs',
+            parmas: {
+              prefix: prefix + '-',
+              minify: true
+            }
+          }
+        ]
+      }
+    }))
+    .pipe(svgStore())
+    .pipe(dest(pathFiles.img.build));
+}
+
+const defaultGulp = series(HTML, SCSS, js, images, fonts, favicon, myServer);
+
+export {HTML}
+export {SCSS}
+export {js}
+export {images}
+export {fonts}
+export {favicon}
+export {myServer}
+export {svgSprite}
+//export { jsVendor }
+
+export {defaultGulp}
